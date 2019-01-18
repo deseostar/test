83fea9d906995a3e2c82b0c7b4fd806228163780
K <K@K306>
2019-01-18 17:56:46 +0900

    수업 빠름빠름

--- JAVA/src/ch01/HelloGit2.java
+++ JAVA/src/ch01/HelloGit2.java
@@ -2,6 +2,7 @@ package ch01;
 
 public class HelloGit2 {
 	public static void main(String[] args) {
-		System.out.println("되는건가?");
+		System.out.println("hell");
+		System.out.println("hell2");
 	}
 }
